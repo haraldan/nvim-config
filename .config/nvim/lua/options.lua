a2778fba38216f9ad7bfc83fd2b5001c3e4a7e4d
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-03-22 16:42:05 +0100

    Update shortcut mappings, nvim-tree file focus behaviour

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,7 +6,7 @@ vim.g.mapleader = " "
 vim.opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-vim.opt.tabstop = 2 -- spaces for tabs (prettier default)
+vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2 -- spaces for indent width
 vim.opt.expandtab = true -- expand tab to spaces
 vim.opt.autoindent = true -- copy indent from current line when starting new one
@@ -14,3 +14,19 @@ vim.opt.autoindent = true -- copy indent from current line when starting new one
 -- ignore case when searching
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- Normal mode shortcuts
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.keymap.set("n", "<leader>cd", ":lcd %:h<CR>") -- change cwd to current file
+vim.keymap.set("n", "<leader>pw", ":pwd<CR>") -- check cwd
+vim.keymap.set("n", "<C-s>", ":w<CR>") -- save file
+
+-- Insert mode shortcuts
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-k>", "<Up>")
+vim.keymap.set("i", "<C-l>", "<Right>")
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>")
+
+-- change update time for CursorHold events
+vim.opt.updatetime = 2000
--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,19 +1,24 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    -- disable netrw at the very start of your init.lua
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-    -- optionally enable 24-bit colour
-    vim.opt.termguicolors = true
-    -- empty setup using defaults
-    require("nvim-tree").setup {}
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		-- disable netrw at the very start of your init.lua
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		-- optionally enable 24-bit colour
+		vim.opt.termguicolors = true
+		-- empty setup using defaults
+		require("nvim-tree").setup({
+			update_focused_file = {
+				enable = true,
+				-- update_root = true,
+			},
+			respect_buf_cwd = true,
+		})
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-
-  end
+		keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+	end,
 }
