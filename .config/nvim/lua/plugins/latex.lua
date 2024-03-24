898733d50b114921f55d4e9463e731feab92ba91
Daniel Sinev <daniel.sinev@gmail.com>
2024-03-24 21:47:24 +0100

    add script to launch sumatraPDF with forward search

--- .config/nvim/lua/plugins/latex.lua
+++ .config/nvim/lua/plugins/latex.lua
@@ -3,7 +3,7 @@ return {
 	init = function()
 		-- Use init for configuration, don't use the more common "config".
 		vim.g.vimtex_compiler_latexmk = { aux_dir = "build/" }
-		vim.g.vimtex_view_general_viewer = "sumatraPDF"
+		vim.g.vimtex_view_general_viewer = "open_sumatrapdf.sh"
 		vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
 	end,
 }
