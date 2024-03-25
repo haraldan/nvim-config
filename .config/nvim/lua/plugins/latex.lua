178962d0d614eac8110028c9639052c4b2c10981
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-03-25 10:31:18 +0100

    remove aux_dir, should fix sumatraPDF not updating

--- .config/nvim/lua/plugins/latex.lua
+++ .config/nvim/lua/plugins/latex.lua
@@ -2,7 +2,7 @@ return {
 	"lervag/vimtex",
 	init = function()
 		-- Use init for configuration, don't use the more common "config".
-		vim.g.vimtex_compiler_latexmk = { aux_dir = "build/" }
+		-- vim.g.vimtex_compiler_latexmk = { aux_dir = "build/" }
 		vim.g.vimtex_view_general_viewer = "open_sumatrapdf.sh"
 		vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
 	end,
