a1af34144ff4107a164708cd060d23c8ffdc1068
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-03-27 12:42:04 +0100

    add indent-blankline for vhdl files

---
+++ .config/nvim/lua/plugins/indent-lines.lua
@@ -0,0 +1,9 @@
+return {
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+    -- enabled = false;
+    ft = {"vhdl"},
+		main = "ibl",
+		opts = {},
+	},
+}
