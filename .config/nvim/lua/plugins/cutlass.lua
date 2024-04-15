7879d3fc5bfd39de15d1ac2c02925e2e08666197
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-04-15 11:17:23 +0200

    enable m2m, etc for cutting lines

--- .config/nvim/lua/plugins/cutlass.lua
+++ .config/nvim/lua/plugins/cutlass.lua
@@ -1,7 +1,16 @@
 return {
-	"gbprod/cutlass.nvim",
-	opts = {
-		cut_key = 'm',
-		override_del = true,
-	},
+  "gbprod/cutlass.nvim",
+  -- opts = {
+  --   cut_key = "m",
+  --   override_del = true,
+  -- },
+  config = function()
+    require("cutlass").setup({
+      cut_key = "m",
+      override_del = true,
+    })
+    for i = 1, 100, 1 do -- enable m2m etc.
+      vim.keymap.set("n", string.format("m%sm",tostring(i)),  string.format("d%sd",tostring(i)), {})
+    end
+  end,
 }
