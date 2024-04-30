44c51a31afe581044fc7b6787c0413baddb93a31
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-04-30 13:36:32 +0200

    swapped sentence and subword text objects

--- .config/nvim/lua/plugins/text_objects.lua
+++ .config/nvim/lua/plugins/text_objects.lua
@@ -5,6 +5,15 @@ return {
 	{
 		"chrisgrieser/nvim-various-textobjs",
 		lazy = false,
-		opts = { useDefaultKeymaps = true },
+		config = function()
+			require("various-textobjs").setup({ useDefaultKeymaps = true })
+			-- Remap sentence text objects
+			vim.keymap.set({ "o", "x" }, "aS", "as", {desc = "outer Sentence"})
+			vim.keymap.set({ "o", "x" }, "iS", "is", {desc = "inner Sentence"})
+			-- example: `as` for outer subword, `is` for inner subword
+			vim.keymap.set({ "o", "x" }, "as", '<cmd>lua require("various-textobjs").subword("outer")<CR>', {desc = "outer subword"})
+			vim.keymap.set({ "o", "x" }, "is", '<cmd>lua require("various-textobjs").subword("inner")<CR>', {desc = "inner subword"})
+		end,
 	},
 }
+
