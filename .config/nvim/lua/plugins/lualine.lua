fdd7ff954a61b466cb496df161e359e02082ee39
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-04-30 12:56:21 +0200

    added auto sessions with Telescope and lualine integration

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,9 +1,13 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {
-    options = {
-      theme = 'auto'
-    },
-  }
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			theme = "auto",
+		},
+		sections = {
+			lualine_c = { require("auto-session.lib").current_session_name, "filename" },
+			lualine_x = { "filetype" },
+		},
+	},
 }
