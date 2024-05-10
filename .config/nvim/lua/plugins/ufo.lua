f5cfe6e1c767a4c4cbcd1acc14eec907585e4131
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-05-10 15:17:55 +0200

    show percentage in folds

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -31,7 +31,9 @@ return {
   config = function(_, opts)
     local handler = function(virtText, lnum, endLnum, width, truncate)
       local newVirtText = {}
-      local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+      local totalLines = vim.api.nvim_buf_line_count(0)
+      local foldedLines = endLnum - lnum
+      local suffix = (" 󰁂 %d [%d%%]"):format(foldedLines, foldedLines / totalLines * 100)
       local sufWidth = vim.fn.strdisplaywidth(suffix)
       local targetWidth = width - sufWidth
       local curWidth = 0
@@ -53,19 +55,14 @@ return {
         end
         curWidth = curWidth + chunkWidth
       end
+      local rAlignAppndx = math.max(math.min(vim.opt.textwidth["_value"], width - 1) - curWidth - sufWidth, 0)
+      suffix = (" "):rep(rAlignAppndx) .. suffix
       table.insert(newVirtText, { suffix, "MoreMsg" })
       return newVirtText
     end
-
-    -- global handler
-    -- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
-    -- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
     opts["fold_virt_text_handler"] = handler
+
     require("ufo").setup(opts)
-    -- buffer scope handler
-    -- will override global handler if it is existed
-    -- local bufnr = vim.api.nvim_get_current_buf()
-    -- require('ufo').setFoldVirtTextHandler(bufnr, handler)
 
     vim.keymap.set("n", "K", function()
       local winid = require("ufo").peekFoldedLinesUnderCursor()
