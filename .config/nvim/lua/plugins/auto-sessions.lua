c884907e14acd98fa4e708b19c0bbb512ed77104
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-05-10 14:35:34 +0200

    Close nvim-tree before saving sessions

--- .config/nvim/lua/plugins/auto-sessions.lua
+++ .config/nvim/lua/plugins/auto-sessions.lua
@@ -4,6 +4,7 @@ return {
 		require("auto-session").setup({
 			log_level = "error",
 			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      pre_save_cmds = { "NvimTreeClose" },
 			session_lens = {
 				-- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
 				buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
