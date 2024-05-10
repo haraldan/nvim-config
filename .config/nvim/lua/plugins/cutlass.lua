274c5f317370caa265baf72f1a6db1e8300e6b55
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-05-10 14:33:02 +0200

    add mark shortcut

--- .config/nvim/lua/plugins/cutlass.lua
+++ .config/nvim/lua/plugins/cutlass.lua
@@ -9,6 +9,10 @@ return {
       cut_key = "m",
       override_del = true,
     })
+
+    -- change mark shortcut to leader-m, since m is now for cutting
+    vim.keymap.set("n", "<leader>m", "m", { desc = "Set mark" })
+
     for i = 1, 100, 1 do -- enable m2m etc.
       vim.keymap.set("n", string.format("m%sm",tostring(i)),  string.format("d%sd",tostring(i)), {})
     end
