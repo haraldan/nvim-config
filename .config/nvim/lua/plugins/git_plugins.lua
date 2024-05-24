f3d5fcdbaff875e304e5571e8da6c548f61341a1
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-05-24 11:24:15 +0200

    improve git hunk navigation

--- .config/nvim/lua/plugins/git_plugins.lua
+++ .config/nvim/lua/plugins/git_plugins.lua
@@ -16,19 +16,13 @@ return {
 
 					-- Navigation
 					map("n", "gh", function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "]c", bang = true })
-						else
-							gitsigns.nav_hunk("next")
-						end
+						gitsigns.nav_hunk("next")
+            vim.cmd.normal("zz")
 					end, "Next hunk")
 
 					map("n", "gH", function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "[c", bang = true })
-						else
 							gitsigns.nav_hunk("prev")
-						end
+            vim.cmd.normal("zz")
 					end, "Previous hunk")
 
 					-- Actions
