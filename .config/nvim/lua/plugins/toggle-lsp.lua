258320cb6dc17732ee0ea8e51791034f3b1b8c66
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-05-31 11:19:45 +0200

    added toggle-lsp mapping

--- .config/nvim/lua/plugins/toggle-lsp.lua
+++ .config/nvim/lua/plugins/toggle-lsp.lua
@@ -2,5 +2,6 @@ return {
   "WhoIsSethDaniel/toggle-lsp-diagnostics.nvim",
   config = function()
     require("toggle_lsp_diagnostics").init()
+    vim.keymap.set("n", "<leader>lt", ":ToggleDiag<CR>", { desc = "[T]oggle [L]SP " })
   end,
 }
