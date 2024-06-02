531836772cd5a9f908f8ebe49b7e94b5ba40dadb
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-06-02 18:39:24 +0200

    disable char mode in flash.nvim

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -2,7 +2,13 @@ return{
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    modes = {
+      char ={
+        enabled = false,
+      }
+    }
+  },
   -- stylua: ignore
   keys = {
     { "<CR>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
