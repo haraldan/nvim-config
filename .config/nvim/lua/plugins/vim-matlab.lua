2f1f267d86e8b73e3a77859f3561ebb0510a1412
Daniel Sinev <daniel.sinev@gmail.com>
2024-06-11 20:46:45 +0200

    update nvim-matlab

--- .config/nvim/lua/plugins/vim-matlab.lua
+++ .config/nvim/lua/plugins/vim-matlab.lua
@@ -1,3 +1,10 @@
 return {
-  "kolbbond/nvim-matlab",
+  "haraldan/nvim-matlab",
+  config = function ()
+    vim.g.matlab_auto_mappings = 0
+    vim.g.matlab_server_launcher = 'tmux'
+    vim.g.matlab_server_split = 'left_30'
+
+    vim.keymap.set("n", "<F5>", ":MatlabCliRunFile<CR>", { desc = "Matlab: Run File" })
+  end
 }
