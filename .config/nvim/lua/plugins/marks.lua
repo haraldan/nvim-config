5a50f443682e76aae45c3f1eee65c38a1787ef20
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-06-11 08:42:50 +0200

    marks: gm & gM mappings

--- .config/nvim/lua/plugins/marks.lua
+++ .config/nvim/lua/plugins/marks.lua
@@ -1,10 +1,13 @@
-return{
-  'chentoast/marks.nvim',
-  config = function ()
-      require'marks'.setup {
-        default_mappings = true,
-        signs = true,
-        mappings = {}
-      }
-  end
+return {
+  "chentoast/marks.nvim",
+  config = function()
+    require("marks").setup({
+      default_mappings = true,
+      signs = true,
+      mappings = {
+        next = "gm",
+        prev = "gM",
+      },
+    })
+  end,
 }
