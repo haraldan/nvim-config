9eeb605f6d00824d4af4c624e6d2fe0abf75d2ac
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-06-14 15:51:12 +0200

    added DAP

---
+++ .config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,34 @@
+return {
+  "rcarriga/nvim-dap-ui",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    "nvim-neotest/nvim-nio",
+    "mfussenegger/nvim-dap-python",
+  },
+  config = function()
+    require("dapui").setup()
+    require("dap-python").setup("python")
+
+    local dap, dapui = require("dap"), require("dapui")
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<Leader>db", ":DapToggleBreakpoint<CR>")
+    vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+    vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+    vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+    -- vim.keymap.set("n", "<Leader>dm", require('dap-python').test_method)
+    -- vim.keymap.set("v", "<Leader>ds", require('dap-python').debug_selection)
+  end,
+}
