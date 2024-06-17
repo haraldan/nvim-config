0cdd0c93a6e0befd079664438cf51233cb433732
Daniel Sinev <daniel.sinev@sennheiser.com>
2024-06-17 11:29:14 +0200

    updated vim-visual-multi mappings

--- .config/nvim/lua/plugins/vim-visual-multi.lua
+++ .config/nvim/lua/plugins/vim-visual-multi.lua
@@ -1,12 +1,19 @@
 return {
 	"mg979/vim-visual-multi",
 	branch = "master",
-  -- enabled = false,
 	init = function()
-    vim.g.VM_leader = '<leader>m'
-	end,
-	config = function()
-    vim.g.VM_theme = 'ocean'
-    vim.g.VM_add_cursor_at_pos_no_mappings = 1
+		vim.g.VM_maps = {
+			["Find Under"] = "<leader>vf",
+      ["Add Cursor Down"] = "<C-Down>" ,
+      ["Add Cursor Up"]   = "<C-Up>" ,
+		}
+		vim.g.VM_theme = "ocean"
+		vim.g.VM_add_cursor_at_pos_no_mappings = 1
+		vim.g.VM_default_mappings = 0
+    vim.keymap.set("n", "<leader>vc", "<Plug>(VM-Add-Cursor-At-Pos)", { desc = "Add cursor" })
+    vim.keymap.set("v", "<leader>vc", "<Plug>(VM-Visual-Cursors)", { desc = "Add cursors" })
+    vim.keymap.set("n", "<leader>v", "<Plug>(VM-Toggle-Mappings)", { desc = "Toggle VM mappings" })
+    vim.keymap.set("n", "<leader>va", "<Plug>(VM-Select-All)", { desc = "VM Select all" })
+    vim.keymap.set("n", "<leader>v/", "<Plug>(VM-Start-Regex-Search)", { desc = "VM Regex Search" })
 	end,
 }
